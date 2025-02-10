a08bcb292fa02d9bd0ebcf86c592128c95613aca
Seagull2004 <sea2004gull@gmail.com>
2025-02-10 21:51:44 +0100

    aggiunto script per tentare di rinominare correttamente i film stile papi

---
+++ rename_tools/genre_remover_on_films_title.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# "ANIMAZIONE 2011 (08) - rango.mp4" => "rango (2011).mp4"
+
+find $1 -type f -exec basename {} \; | while read -r file
+do
+    year=`echo $file | cut -d "(" -f1 | rev`
+    year=${year:0:5}
+    year=`echo $year | rev`
+
+    title_and_extension=`echo $file | cut -d "-" -f2- | tr "A-Z" "a-z"`
+    title=`echo $title_and_extension | rev | cut -d. -f2- | rev`
+    extension=`echo $title_and_extension | rev | cut -d. -f1 | rev`
+
+
+    echo "$title ($year).$extension"
+done
