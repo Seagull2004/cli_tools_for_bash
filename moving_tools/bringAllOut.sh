30492f285957d96f16b5fc3403cb271279be663c
Seagull2004 <sea2004gull@gmail.com>
2025-03-30 12:32:35 +0200

    migliorata ux del bring all out

--- moving_tools/bringAllOut.sh
+++ moving_tools/bringAllOut.sh
@@ -1,14 +1,18 @@
-# !/bin/bash
+#!/bin/bash
 # il percorso di una cartella come parametro
 # lo script porterà tutti i file annidati fuori dalle cartelle
 # $1 cartella root
 
 
-if [[ "$1" == "" ]]; then
-	echo "usage: command {pathname}"
-	exit
+if [[ $# < 1 ]]; then
+	echo "usage: command [dir]"
+    echo "e vedrai che tutti i file annidati nelle sottocartelle di [dir] verranno spostati in [dir]"
+	exit 0
 fi
 
-cd "$1"
-find . -mindepth 2 -type f -exec mv {} . \;
+if [[ ! -d $1 ]]; then
+    echo "fornire una cartella pls"
+    exit 1
+fi
 
+find "$1" -mindepth 2 -type f -exec mv {} "$1" \;
